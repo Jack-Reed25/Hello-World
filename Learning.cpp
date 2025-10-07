46d59fab7c987a8a088f8c3e02b6fd1dc2bc2b3c
Jack Reed <jack.reed25@bathspa.ac.uk>
2025-10-07 12:11:42 +0100

    Update Learning.cpp

--- Learning.cpp
+++ Learning.cpp
@@ -1,37 +1,3 @@
-# Hello-World
-
-// class notes
-
-#include <iostream> 
-using namespace std;
-int main(){ 
-  int myNumber;
-  myNumber = 5;
-
-
-  doubble price = 49.99;
-  float hight = 162.5;
-  char initial = 'H';
-  bool isAwake = false; 
-
-  i got traller
-  return 0 ;
-}
-
-
-#include <iostream> 
-using namespace std;
-
-int main () {
-
-
-
-// Notes
-
-#include <iostream>
-#include <string>
-using namespace std;
-
 int main() {
     firstNumber = 1;
     iCanCode = true;
