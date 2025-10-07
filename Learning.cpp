d39fc32079d8da9dccb6a9c229ad6c409130df07
Jack Reed <jack.reed25@bathspa.ac.uk>
2025-10-07 12:05:11 +0100

    Update Learning.cpp

--- Learning.cpp
+++ Learning.cpp
@@ -23,3 +23,23 @@ int main(){
 using namespace std;
 
 int main () {
+
+
+
+// Notes
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    firstNumber = 1;
+    iCanCode = true;
+    hopefulGrade = 'a';
+    myDecimal = 1.0;
+    minimalSentance = "y";
+    keyMash = 13213123;
+    mysteryDataType = 5.6f; //Use Google to work this out
+
+    return 0;
+}
