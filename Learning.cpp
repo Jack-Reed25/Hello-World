07562255a3d0e3e8681e38da6c2c0e80ca817807
Jack Reed <jack.reed25@bathspa.ac.uk>
2025-10-07 12:59:42 +0100

    commit

--- Learning.cpp
+++ Learning.cpp
@@ -1,13 +1,66 @@
-int main() {
-    firstNumber = 1;
-    iCanCode = true;
-    hopefulGrade = 'a';
-    myDecimal = 1.0;
-    minimalSentance = "y";
-    keyMash = 13213123;
-    mysteryDataType = 5.6f; //Use Google to work this out
 
-    return 0;
+
+//class notes 
+
+
+#include <iostream>  // input & output functionality
+#include <string>
+using namespace std; // declares used of std namespace
+
+int main() {
+   int  firstNumber = 1;
+   bool  iCanCode = true;
+   char hopefulGrade = 'a';
+   double  myDecimal = 1.0;
+   char  minimalSentance = "y"; // becuse of double quotes
+   int  keyMash = 13213123;
+   float mysteryDataType = 5.6f; //Use Google to work this out
+
+    return 0; // return statement - terminates program
 }
 
 
+
+
+
+#include <iostream>
+using namespace std;
+
+int mai. () {
+cout << "stuff" << endl;
+
+int age. = 99
+cout << "your age is" << age << endl
+
+float hight = 182.5
+cout << "your hight is" << hight << endl
+
+cout << "this is verry"
+     << "wery wery wery wery"
+     << "long count" << endl;
+
+cout 
+}
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Please enter yoru name" << endl;
+
+    strign name;
+    cin >> name; 
+
+    cout << "hello, "<< name << "you are the best." <<endl;
+
+int faveNumber;
+cout << "please enter your favorite number" << endl;
+cin >> faveNumber;
+
+cout << "no that is a rubbish chouce" << endl; 
+
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Hello world 
-hello 
-m
+#include <iostream> 
+using namespace std;
+int main(){ 
+  int myNumber;
+  myNumber = 5;
+
+
+  doubble price = 49.99;
+  float hight = 162.5;
+  char initial = 'H';
+  bool isAwake = false; 
+
+  i got traller
+  return 0 ;
+}
