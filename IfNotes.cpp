abfe121f73b7a95de66250d27d4647e87f0d645b
Jack Reed <jack.reed25@bathspa.ac.uk>
2025-10-17 12:05:21 +0100

    Correct voting eligibility logic in IfNotes.cpp
    Fix syntax errors and improve voting eligibility check.

--- IfNotes.cpp
+++ IfNotes.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+
+ double Age;
+    cout << "What is your Age)? ";
+    cin >> Age;
+
+if (Age > 17) {
+    cout << "You are eligible to vote " ;
+  }
+if else 
+  cout << "sorry you are not old enough to vote.";
+
+  return 0;
+}
